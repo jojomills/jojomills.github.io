012461115135b6f2ab5c6b886dc8a2f676b7b4f3
Jojo Mills <mills.josephine@gmail.com>
2020-06-18 13:27:28 -0700

    new html pages for each tab and a second css style sheet. also adds all photos. June 18, 2020

--- website.js
+++ website.js
@@ -1,1 +1,45 @@
-console.log('Hello');
+/*
+Josephine Mills
+Personal Website
+Created June 12, 2020
+Last updated June 12, 2020
+
+
+*/
+
+(function(){
+
+	"use strict";
+	window.onload = function () {
+		document.getElementById("homeButton").onclick = home;
+		document.getElementById("aboutButton").onclick = about;
+		document.getElementById("educationButton").onclick = education;
+		document.getElementById("experienceButton").onclick = experience;
+		document.getElementById("skillsButton").onclick = skills;
+		document.getElementById("contactButton").onclick = contact;
+		//add different layouts
+
+	}
+
+	function home(){
+		//other page goes invisible
+		document.getElementById("main").style.visibility = "hidden";
+	}
+	function about(){
+		
+	}
+	function education(){
+		
+	}
+	function experience(){
+		
+	}
+	function skills(){
+		
+	}
+	function contact(){
+		
+	}
+
+
+}) ();
